4876189d0f8835365adf48c609cb2fb5dd2d545c
Parlerler <malekwa3er99@gmail.com>
2023-08-16 19:51:50 +0300

    push trial

--- main.cs
+++ main.cs
@@ -2,11 +2,26 @@
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
+    class person
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public person(string name = "lion")
+        {
+            Name = name;
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello please work again!");
+            //Console.WriteLine("Hello please work again!");
+            person guy = new person();
+            Console.WriteLine(guy.Name);
         }
     }
 }
