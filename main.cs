62cfb419beafc4aa3e20603d4ea2f51ce3885181
Kareem Essam <kareemessam2101@gmail.com>
2023-08-17 14:55:54 +0300

    Created basic classes for user , rider , driver.

--- main.cs
+++ main.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello please work again!");
+           Location test= new Location(1.2,3.2);
+           Rider one= new  Rider("Kareem","Essam","kareemessam2101@gmail.com","testtube","01092217001",test);
+           Console.WriteLine(one.FirstName);
+           Console.WriteLine(one.LastName);
+           Console.WriteLine(one.Email);
+           Console.WriteLine(one.Password);
+           Console.WriteLine(one.PhoneNumber);
+           Console.WriteLine(one.CurrentLocation.Latitude);
+           Console.WriteLine(one.CurrentLocation.Longitude);
         }
     }
+    
 }
---
+++ user.cs
@@ -0,0 +1,70 @@
+using System;
+public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public User(string firstName, string lastName, string email, string password, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+        }
+    }
+
+    public class Rider : User
+    {
+        public Location CurrentLocation { get; set; }
+
+        public Rider(string firstName, string lastName, string email, string password, string phoneNumber, Location currentLocation)
+            : base(firstName, lastName, email, password, phoneNumber)
+        {
+            CurrentLocation = currentLocation;
+        }
+
+        // public void RequestRide(Location destination)
+        // {
+        //     Console.WriteLine($"{FirstName} {LastName} is requesting a ride to {destination}.");
+        //     // Logic to request a ride goes here
+        // }
+
+        // public void CancelRide()
+        // {
+        //     Console.WriteLine($"{FirstName} {LastName} has canceled the ride.");
+        //     // Logic to cancel a ride goes here
+        // }
+    }
+
+    public class Driver : User
+    {
+        public Car DriverCar { get; set; }
+
+        public Driver(string firstName, string lastName, string email, string password, string phoneNumber, Car driverCar)
+            : base(firstName, lastName, email, password, phoneNumber)
+        {
+            DriverCar = driverCar;
+        }
+
+        // public void AcceptRideRequest(Rider rider, Location destination)
+        // {
+        //     Console.WriteLine($"{FirstName} {LastName} is accepting a ride request from {rider.FirstName} to {destination}.");
+        //     // Logic to accept a ride request goes here
+        // }
+
+        // public void StartRide()
+        // {
+        //     Console.WriteLine($"{FirstName} {LastName} has started the ride.");
+        //     // Logic to start a ride goes here
+        // }
+
+        // public void EndRide()
+        // {
+        //     Console.WriteLine($"{FirstName} {LastName} has ended the ride.");
+        //     // Logic to end a ride goes here
+        // }
+    }
