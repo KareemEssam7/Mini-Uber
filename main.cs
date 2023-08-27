bd567dbf4ebb255b3eb0ca609de4097a5e57d871
Parlerler <malekwa3er99@gmail.com>
2023-08-27 18:53:16 +0300

    work on payment strategy

--- main.cs
+++ main.cs
@@ -32,6 +32,8 @@ namespace MyApp
             ///////////////////////////////////////////////////
 
             User activeUser = new User();
+
+
             IHandler loginHandler = new LoginHandler();
             IHandler registerHandler = new RegisterHandler();
 
@@ -39,6 +41,28 @@ namespace MyApp
             Console.WriteLine("Register or Login?");
             string enteraction = Console.ReadLine()!;
             Console.WriteLine(loginHandler.HandleRequest(enteraction, activeUser, cs));
+
+            string paymentMethod = CreditMethods.GetUserPaymentMethod();
+            IPaymentStrategy paymentStrategy;
+
+            if (paymentMethod == "creditcard")
+            {
+                paymentStrategy = new CreditCardPayment("parlerler", "1010010101", 0, 0, "033", 0.0);
+                CreditCardPayment activeCredit = new CreditCardPayment("parlerler", "1010010101", 0, 0, "033", 0.0);
+                activeCredit.registerCreditInfo();
+            }
+            else if (paymentMethod == "paypal")
+            {
+                Console.WriteLine("Enter paypal Email");
+                string email = Console.ReadLine()!;
+                paymentStrategy = new PayPalPayment(email);
+            }
+            else if (paymentMethod == "cash")
+            {
+                Console.WriteLine("Thank you");
+            }
+            //should only take this information when regestering user.
+            //should register this info into the database with the rest of the user info.
         }
     }
 }
--- paymentContext.cs
+++ paymentContext.cs
@@ -1,42 +1,92 @@
-public abstract class Strategy
+public interface IPaymentStrategy
 {
-    public abstract void paymentMethod();
+    void ProcessPayment(double amount);
 }
 
-public class creditCardMethod : Strategy
+public class CreditCardPayment : IPaymentStrategy
 {
-    public override void paymentMethod()
+    public string CreditName { get; set; }
+    public string CreditNumber { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+    public string CreditCVV { get; set; }
+    public double CreditBalance { get; set; }
+
+    public CreditCardPayment(string creditName, string creditNumber, int month, int year, string creditCVV, double creditBalance)
     {
-        //add logic
+        CreditName = creditName;
+        CreditNumber = creditNumber;
+        Month = month;
+        Year = year;
+        CreditCVV = creditCVV;
+        CreditBalance = creditBalance;
+    }
+    public void ProcessPayment(double amount)
+    {
+        //Console.WriteLine($"Processing credit card payment of ${amount} using card number {cardNumber}.");
+        // Actual payment processing logic here
+    }
+    public void registerCreditInfo()
+    {
+        do
+        {
+            Console.WriteLine("enter credit name: ");
+            CreditName = Console.ReadLine()!;
+        } while (!Validate.ValidateName(CreditName));
+
+        do
+        {
+            Console.WriteLine("enter credit number: ");
+            CreditNumber = Console.ReadLine()!;
+        } while (!Validate.ValidateCreditCardNumber(CreditNumber));
+
+        do
+        {
+            Console.WriteLine("enter expiry month and year: ");
+            Month = Convert.ToInt32(Console.ReadLine());
+            Year = Convert.ToInt32(Console.ReadLine());
+        } while (!Validate.ValidateExpirationDate(Month, Year));
+
+        do
+        {
+            Console.WriteLine("enter CVV: ");
+            CreditCVV = Console.ReadLine()!;
+        } while (!Validate.ValidateCVV(CreditCVV));
+    }
+
+}
+public static class CreditMethods
+{
+    public static string GetUserPaymentMethod()
+    {
+        Console.WriteLine("Enter your desired payment method\n 1: CreditCard\n 2: PayPal\n 3: Cash");
+        string interaction = Console.ReadLine()!;
+        return interaction;
+    }
+
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    private string email;
+
+    public PayPalPayment(string email)
+    {
+        this.email = email;
+    }
+
+    public void ProcessPayment(double amount)
+    {
+        Console.WriteLine($"Processing PayPal payment of ${amount} using email {email}.");
+        // Actual payment processing logic here
     }
 }
 
-public class payPalMethod : Strategy
+public class CashPayment : IPaymentStrategy
 {
-    public override void paymentMethod()
+    public void ProcessPayment(double amount)
     {
-        //add logic
+        Console.WriteLine("Please pay the driver in person");
     }
-}
 
-public class cashMethod : Strategy
-{
-    public override void paymentMethod()
-    {
-        //add logic
-    }
-}
-
-public class Context
-{
-    Strategy strategy;
-    // Constructor
-    public Context(Strategy strategy)
-    {
-        this.strategy = strategy;
-    }
-    public void ContextInterface()
-    {
-        strategy.paymentMethod();
-    }
 }
